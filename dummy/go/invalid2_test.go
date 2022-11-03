4c864d07a92308ed3274d2fd99812db016db3e12
Jonathan Ryadi <34571775+verlandz@users.noreply.github.com>
2022-11-03 19:35:27 +0700

    PoC file changes (1)

---
+++ dummy/go/invalid2_test.go
@@ -0,0 +1,43 @@
+package dummy_go
+
+import (
+	"testing"
+)
+
+func Test_getA(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "sample",
+			want: "A",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getA(); got != tt.want {
+				t.Errorf("getA() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getB(t *testing.T) {
+	tests := []struct {
+		name string
+		want int
+	}{
+		{
+			name: "sample",
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getB(); got != tt.want {
+				t.Errorf("getB() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
