96c646adee1914a1c7d2a7b4d0ac5556b8d564d1
verlandz <verlandz@users.noreply.github.com>
2022-12-01 19:05:43 +0000

    [BOT] Autofix | Go Formatter

--- dummy-autofix/go/invalid2.go
+++ dummy-autofix/go/invalid2.go
@@ -5,23 +5,19 @@ import (
 	"log"
 )
 
-func 		getA() string {
-	res :=    "A"
+func getA() string {
+	res := "A"
 	fmt.Println(res)
-log.Println(res)
-	return   res
+	log.Println(res)
+	return res
 }
 
 func getB() int {
 	res := 2
 
-
-
-		fmt.Println(res)
-	
+	fmt.Println(res)
 
 	log.Println(res)
 
-return res
+	return res
 }
-
